4932a96f42ea9f0f3d315bf3bb6d4eaebd925cb3
kifran <cr91700kifran@gmail.com>
2019-04-02 03:01:13 +0200

    d

--- public/js/worldchat/chat.js
+++ public/js/worldchat/chat.js
@@ -6,7 +6,7 @@ app.controller('worldchat', function($scope, $http, $window) {
 
   let id_user = angular.element('.userinfo')[0].id;
   // let socket = io.connect('localhost:3000');
-  let socket = io.connect('51.38.236.160:80');
+  let socket = io.connect('localhost:80');
 
 
   $http.get('worldchat/getMessages').then((response) => {
